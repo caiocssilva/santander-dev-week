c1441d39c314d7a68e5f366fbbfa87cfa371590d
Igo <igo.vieira@live.com>
2024-06-04 13:23:21 -0300

    fix: precision e scale

--- src/main/java/me/dio/model/Card.java
+++ src/main/java/me/dio/model/Card.java
@@ -14,7 +14,7 @@ public class Card {
     @Column(nullable = false, unique = true)
     private String number;
 
-    @Column(name = "available_limit", nullable = false)
+    @Column(name = "available_limit", nullable = false, precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Card(){
